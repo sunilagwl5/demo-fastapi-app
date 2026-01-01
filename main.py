a78bd1d20bb477ae9a2d97db23ccdb232351e411
Sunil Agarwal <sunilagwl5@users.noreply.github.com>
2026-01-01 10:02:59 +0000

    Update default message

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello Perfectus!"}
+    return {"message": "Hello World!"}
 
 if __name__ == "__main__":
     import uvicorn
