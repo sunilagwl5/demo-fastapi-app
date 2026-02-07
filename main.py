0499d7dbbd8a6d61b9760ca4ae8fd3117648a054
Sunil Agarwal <sunilagwl5@users.noreply.github.com>
2026-02-07 18:38:08 +0000

    Update greeting to Happy Independence Day

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def read_index():
 
 @app.get("/api")
 async def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Happy Independence day"}
 
 # -- Item Endpoints --
 
