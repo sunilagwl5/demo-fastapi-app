0e816e7fbe974b3ec1a439c3b64b9ac4e4edcbd9
Sunil Agarwal <sunilagwl5@users.noreply.github.com>
2026-02-15 14:04:30 +0000

    Update main message to 'Hello vibedoctor

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def read_index():
 
 @app.get("/api")
 async def read_root():
-    return {"message": "Hello to vibedoctor Demo"}
+    return {"message": "Hello vibedoctor"}
 
 # -- Item Endpoints --
 
